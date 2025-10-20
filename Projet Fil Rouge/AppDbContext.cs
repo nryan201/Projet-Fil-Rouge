8876cc249f5d81576b64597c3317e9fd389438ce
nryan <ryan.nedjari@ynov.com>
2025-10-20 15:23:32 +0200

    Ajoutez des fichiers projet.

---
+++ Projet Fil Rouge/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Projet_Fil_Rouge
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
